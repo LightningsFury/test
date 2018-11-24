5a6c2c8ed760ec9aa48b917e50078d6d70c3e9f2
Lightning's Fury <light@memeware.net>
2018-11-24 20:31:46 +0000

    some customisation

--- server.js
+++ server.js
@@ -2,23 +2,23 @@
 // where your node app starts
 
 // init project
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// we've started you off with Express, 
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 
 // http://expressjs.com/en/starter/basic-routing.html
 
-app.use(express.static('views'))
+app.use(express.static("views"));
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
 });
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen("3000", function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
