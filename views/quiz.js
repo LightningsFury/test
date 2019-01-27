31df407412dbecbc352cbaa6ebea74d7f82907b5
LightningsFury <light@memeware.net>
2019-01-27 19:08:15 +0000

    major edits for salary page

--- views/quiz.js
+++ views/quiz.js
@@ -1,1 +1,56 @@
-function getConditions() {}
+function conditionInfo(condition) {
+  $(function() {
+    $.ajax({
+      url: "https://api.nhs.uk/conditions/" + condition,
+      beforeSend: function(xhrObj) {
+        xhrObj.setRequestHeader(
+          "subscription-key",
+          "ab8469f0df524f2bab0f756beeb44a43"
+        );
+      },
+      type: "GET",
+      data: "{body}"
+    })
+      .done(console.log)
+      .fail(console.error);
+  });
+}
+
+function getConditions(page) {
+  if (!page) page = 1;
+  var res = $(function() {
+    var tres;
+    $.ajax({
+      url: "https://api.nhs.uk/conditions/?page=" + page,
+      beforeSend: function(xhrObj) {
+        xhrObj.setRequestHeader(
+          "subscription-key",
+          "ab8469f0df524f2bab0f756beeb44a43"
+        );
+      },
+      type: "GET",
+      data: "{body}"
+    })
+      .done(function(data) {
+        console.log(
+          data.significantLink.map(function(a) {
+            return {
+              name: a.name,
+              description: a.description,
+              url: a.url
+            };
+          })
+        );
+      })
+      .fail(error);
+    return;
+  });
+  return res;
+}
+
+function error() {
+  document.getElementById("content").innerHTML =
+    "<b>Unfortunately there has been an error, please refresh the page. Sorry for any inconvenience caused.</b>";
+}
+
+conditionInfo("measles");
